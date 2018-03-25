a7b1514763206b0d2642c078138747b5482426b5
Victoria Meng <nights1ststar@protonmail.com>
2018-03-25 20:52:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,6 @@ class Song
     file_array = file.split(%r{ - })
     file_array[-1].slice!(".mp3")
     song = Song.new(file_array[1])
-    artist = Artist.find_or_create_by_name(file_array[0])
-    # artist.add_song(song)
-    # song
   end
 
   def self.artist_name(artist_name)
